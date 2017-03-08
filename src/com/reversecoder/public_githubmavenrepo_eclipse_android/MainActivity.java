f44ffd3e32077b11cd830cad74cc734bb530a23d
Md. Rashadul Alam <rashedul.alam@kuusoft.com>
2017-03-09 00:59:42 +0600

    rc-public-githubmavenrepo-eclipse-android: project initialization

---
+++ src/com/reversecoder/public_githubmavenrepo_eclipse_android/MainActivity.java
@@ -0,0 +1,13 @@
+package com.reversecoder.public_githubmavenrepo_eclipse_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
